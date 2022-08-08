bafed0868310ed9a20fd5419653a7044626bc995
mackliu <mackliu@gmail.com>
2022-08-08 13:53:15 +0800

    將資料顯示在列表區塊上對應的欄位

--- back/movie.php
+++ back/movie.php
@@ -1,18 +1,22 @@
 <button onclick="location.href='?do=add_movie'">新增電影</button>
 <hr>
 <div style="overflow:scroll;height:430px;">
+<?php
+$rows=$Movie->all(" order by rank");
+foreach($rows as $key => $row){
+?>
     <div style="background:#eee;width:99%;height:140px;margin:2px 0;display:flex;">
         <div style="width:15%">
-            <img src="" alt="">dfasf
+            <img src="./upload/<?=$row['poster'];?>" style="height:130px;">
         </div>
         <div style="width:15%">
-            分級:<img class="img-fluid" src="" alt="">
+            分級:<img src="./icon/<?=$Level[$row['level']];?>" alt="">
         </div>
         <div style="width:70%">
             <div style="display:flex">
-                <div style="width:33.33%">片名:</div>
-                <div style="width:33.33%">片長:</div>
-                <div style="width:33.33%">上映時間</div>
+                <div style="width:33.33%">片名:<?=$row['name'];?></div>
+                <div style="width:33.33%">片長:<?=$row['length'];?></div>
+                <div style="width:33.33%">上映時間:<?=$row['ondate'];?></div>
             </div>
             <div>
                 <button>顯示</button>
@@ -22,12 +26,15 @@
                 <button>刪除電影</button>
             </div>
             <div>
-                劇情介紹:
+                劇情介紹:<?=$row['intro'];?>
             </div>
         </div>
     </div>
+<?php
+}
+?>    
 </div>
 <script>
 
-    
+
 </script>
